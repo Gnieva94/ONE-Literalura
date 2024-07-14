eee5011f9d15b19fa4cd2f524106d8c6440c7cc2
Gnieva94 <gus_94@hotmail.com.ar>
2024-07-14 04:12:09 -0300

    Inicio de proyecto de consola literalura con Java Spring

---
+++ src/test/java/com/gnievassj/literarula/LiterarulaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gnievassj.literarula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiterarulaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
